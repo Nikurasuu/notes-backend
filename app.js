994a037cd4daff323bdcea5be1a99e425a56f9f8
Niklas Malkusch <niklasmalkusch@googlemail.com>
2024-05-27 17:30:27 +0200

    Implemented GET /notes

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const Note = require("./note.js")
 
 const notesStore = new NotesStore();
 
-const notiz1 = new Note("test1", "inhalt1")
+/*const notiz1 = new Note("test1", "inhalt1")
 const notiz2 = new Note("test2", "inhalt2")
-
 notesStore.addNote(notiz1)
-notesStore.addNote(notiz2)
-
-console.log(notesStore.getNotes())
+notesStore.addNote(notiz2)*/
 
 var express = require("express");
 var app = express();
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
     res.send(notesStore.getNotes())
 })
 
-//benutzt port 3000 
+// Startet server mit dem Port 3000
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 }); 
